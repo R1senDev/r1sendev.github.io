dd1006506a658d92b1bafcb819ce6d45420592a6
Pavel Ovchinnikov <99139074+R1senDev@users.noreply.github.com>
2024-07-01 19:55:38 +0300

    Fixed my crappy code

--- to_you/script.js
+++ to_you/script.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
     counterElem = document.getElementById('counter');
     box1 = document.getElementById('box1');
     box2 = document.getElementById('box2');
-    box2.style.transition = 'opacity cubic-bezier(0.075, 0.82, 0.165, 1) 1.5s';
 });
 
 
