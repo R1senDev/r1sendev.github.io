76055f81a2eb696a5e86f4a32dd33562dd9efb0d
Anna Romashina <anny.200@proton.me>
2026-08-29 15:36:36 +0300

    Initial commit

--- cringe.js
+++
@@ -1,3 +0,0 @@
-const url = new URL(location.href);
-
-if (!url.searchParams.get('via_button')) location.href = 'nope.html';
--- home.js
+++
@@ -1,3 +0,0 @@
-const url = new URL(location.href);
-
-if (url.searchParams.get('cheater')) document.getElementById('cheaterplace').innerHTML = ', <span class="cheater">dirty cheater!</span>'
